9dc9a7d225fe146e5e7efb8c440839ce3bd43c18
krzysztof-galazka <kgalazka@vp.pl>
2020-05-05 01:41:46 +0200

    Space invaders

---
+++ src/Ship.java
@@ -0,0 +1,31 @@
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+
+public class Ship extends Rectangle {
+    boolean dead = false;
+    final String type;
+
+    Ship(int x, int y, int w, int h, String type, Color color) {
+        super(w, h, color);
+
+        this.type = type;
+        setTranslateX(x);
+        setTranslateY(y);
+    }
+
+    void moveLeft() {
+        setTranslateX(getTranslateX() - 5);
+    }
+
+    void moveRight() {
+        setTranslateX(getTranslateX() + 5);
+    }
+
+    void moveUp() {
+        setTranslateY(getTranslateY() - 5);
+    }
+
+    void moveDown() {
+        setTranslateY(getTranslateY() + 5);
+    }
+}
